a1924e457950368bc40af92d08645ea98cbfe18d
soadfreyle <soadfre@gmail.com>
2022-12-20 14:18:39 -0500

    guardado, dom, borrar

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_ipwiI7pbcqPZuGeL1p6cJQDqImSxvgn9Oyyd0x83jwpATwTkvPPYpeN9Ae4yaZgt';
-const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=3&api_key=live_ipwiI7pbcqPZuGeL1p6cJQDqImSxvgn9Oyyd0x83jwpATwTkvPPYpeN9Ae4yaZgt';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?api_key=live_ipwiI7pbcqPZuGeL1p6cJQDqImSxvgn9Oyyd0x83jwpATwTkvPPYpeN9Ae4yaZgt';
+const API_URL_FAVORITES_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}?api_key=live_ipwiI7pbcqPZuGeL1p6cJQDqImSxvgn9Oyyd0x83jwpATwTkvPPYpeN9Ae4yaZgt`;
+
+
+
 
 const spanError = document.getElementById('error');
 
@@ -16,10 +20,17 @@ async function LoadRandomMichis() {
         const img1 = document.getElementById('img1');
         const img2 = document.getElementById('img2');
         const img3 = document.getElementById('img3');
+        const btn1 = document.getElementById('btn1');
+        const btn2 = document.getElementById('btn2');
+        const btn3 = document.getElementById('btn3');
     
         img1.src = data[0].url;
         img2.src = data[1].url;
         img3.src = data[2].url;
+
+        btn1.onclick = () => saveFavouriteMichi(data[0].id);
+        btn2.onclick = () => saveFavouriteMichi(data[1].id);
+        btn3.onclick = () => saveFavouriteMichi(data[2].id);
     }
 
 
@@ -33,17 +44,41 @@ async function LoadFavouriteMichis() {
 
     if (res.status !== 200) {
         spanError.innerHTML = "Hubo un error" + res.status + data.message; 
+    } else {
+
+        const section = document.getElementById('favouriteMichis');
+
+        section.innerHTML = "";
+        const h2 = document.createElement('h2');
+        const h2Text = document.createTextNode('Michis favoritos');
+        h2.appendChild(h2Text);
+        section.appendChild(h2);
+
+        data.forEach(michi => {
+            const article = document.createElement('article');
+            const img = document.createElement('img');
+            const btn = document.createElement('button');
+            const btnText = document.createTextNode('Sacar al michi de favoritos');
+    
+            img.src = michi.image.url;
+            img.width = 150;
+            btn.appendChild(btnText);
+            btn.onclick = () => deleteFavouriteMichi(michi.id);
+            article.appendChild(img);
+            article.appendChild(btn);
+            section.appendChild(article);
+        });
     }
 }
 
-async function saveFavouriteMichis() {
+async function saveFavouriteMichi(id) {
     const res = await fetch(API_URL_FAVORITES, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            image_id: 'sg'
+            image_id: id
         }),
 
     }); 
@@ -54,8 +89,24 @@ async function saveFavouriteMichis() {
 
     if (res.status !== 200) {
         spanError.innerHTML = "Hubo un error" + res.status + data.message; 
+    } else {
+        console.log('Michi guardado en favoritos')
+        LoadFavouriteMichis();
     }
     
+}
+
+async function deleteFavouriteMichi(id) {
+    const res = await fetch(API_URL_FAVORITES_DELETE(id), {
+        method: 'DELETE',
+    });
+    const data = await res.json();
+    if (res.status !== 200) {
+        spanError.innerHTML = "Hubo un error: " + res.status +data.message;
+    } else {
+        console.log('Michi eliminadp de favourites')
+        LoadFavouriteMichis();
+    }
 }
 
 LoadRandomMichis();
